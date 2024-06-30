8c803defd58d590f8b85a207ef3a384b7a2f85cc
twindai <guandai@hotmail.com>
2024-06-30 11:08:36 +0800

    add getCalculateRate

--- backend/src/routes/shippingRateRoutes.ts
+++ backend/src/routes/shippingRateRoutes.ts
@@ -1,9 +1,14 @@
 // backend/src/routes/shippingRateRoutes.ts
-import { Router } from 'express';
-import { getShippingRates } from '../controllers/shippingRateController';
+import { Router, Request, Response } from 'express';
+import { getRate, getCalculateRate, getShippingRates } from '../controllers/shippingRateController';
 
 const router = Router();
 
 router.get('/', getShippingRates);
+
+
+router.get('/rate', getRate);
+
+router.get('/calculate-rate', getCalculateRate);
 
 export default router;
