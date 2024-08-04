6d0312c390df02fe5e737b4b4895718882f94726
twindai <guandai@hotmail.com>
2024-08-04 20:37:42 +0200

    fix errors

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -10,9 +10,21 @@ export const isValidJSON = (str: string) => {
 	}
 }
 
+export const reducedError = (error: any) => {
+	const stacks = error.stack.split('\n')
+	const lastFnName = stacks.pop().split(' ')[5];
+	const messages = [
+		['name', error.name],
+		['original', error.original],
+		['errors', error.errors.map((e: any) => e.message)],
+		['lastName', lastFnName],
+	];
+	return `\n${messages.map(msg => `ReducedError [${msg[0]}]: ${msg[1]}`).join('\n')}`;
+}
+	
 export const aggregateError = (error: UniqueConstraintError | Error) => 
 	error?.constructor.name === 'UniqueConstraintError' &&  'errors' in error
-		? error.errors.map((e: any) => e.message).join(', ')
+		? error.errors.map((e: any) => `${e.message}, ${error.original.message}`).join(', ')
 		: error?.message;
 
 export const ReturnMsg = <T>(res: ResponseAdv<T>, message: string, code = 400) => res.status(code).json({ message});
