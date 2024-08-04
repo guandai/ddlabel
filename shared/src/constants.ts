1e00490395c64d479e8b19a939f846975dfb6933
twindai <guandai@hotmail.com>
2024-08-04 21:49:12 +0200

    loading

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -6,8 +6,8 @@ const ADDRESS_FROM_KEYS_OPTIONAL: KeyCsvRecord[] = ['fromAddress2'];
 const ADDRESS_TO_KEYS: KeyCsvRecord[] = ['toName', 'toAddress1', 'toAddressZip'];
 const ADDRESS_TO_KEYS_OPTIONAL: KeyCsvRecord[] = ['toAddress2'];
 
-const ROOT_KEYS: KeyCsvRecord[] = ['weight', 'referenceNo'];
-const ROOT_KEYS_OPTIONAL: KeyCsvRecord[] = ['length', 'width', 'height', 'trackingNo'];
+const ROOT_KEYS: KeyCsvRecord[] = ['weight'];
+const ROOT_KEYS_OPTIONAL: KeyCsvRecord[] = ['length', 'width', 'height', 'trackingNo', 'referenceNo'];
 
 export const CSV_KEYS_OPTIONAL = ROOT_KEYS_OPTIONAL.concat(ADDRESS_TO_KEYS_OPTIONAL, ADDRESS_FROM_KEYS_OPTIONAL);
 export const CSV_KEYS_REQUIRED = ROOT_KEYS.concat(ADDRESS_FROM_KEYS, ADDRESS_TO_KEYS);
